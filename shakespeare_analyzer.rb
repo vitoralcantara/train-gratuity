c768931a24050b8d4a8cccfc96e7c257be670602
Vítor Alcântara de Almeida <vitoralcantara@ppgsc.ufrn.br>
2016-04-20 16:46:57 -0300

    Finished first versino of algorithm

--- shakespeare_analyzer.rb
+++ shakespeare_analyzer.rb
@@ -17,71 +17,67 @@ end
 
 class XML_Element_Count
 
-	def self.parse(xml_url,category,child_type,exceptions)
-		xml_url = Nokogiri::XML(open("http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"))		
-		category = category
-		child_type = child_name
+	def self.parse(xml_url)
 		pair_array = Array.new		
 		doc = Nokogiri::XML(open("http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"))
-		category_list = get_categories(doc,category)
-		category_list.each do |category|
-			if not exception(category,exceptions)
-				sum_children(pair_array,get_children_count(category,child_name))
+		speeches = get_speeches(doc)
+		speeches.each do |speech|
+			children = speech.children
+			speaker = get_speaker(children)
+			if not is_all_speaker(speaker)
+				line_count = get_line_count(children)
+				sum_children(pair_array,speaker,line_count)
 			end
+		end
 		return pair_array
+	end
 
 	private 
 
-	def self.get_categories(doc,category)
-		return doc.xpath("//" + category)
+	def self.get_speeches(doc)
+		return doc.xpath("//SPEECH")
 	end
 
-	def self.get_children_count(category,child_name)
-			children = category.children
+	def self.get_speaker(children)
+		return children[1].content
+	end
+
+
+	def self.get_line_count(children)
 			count = 0
-			children.each{|child|
-				if child.name == child_name
-					count++
+			children.each do |child|
+				if child.name == "LINE"
+					count = count+1
 				end
-			}
+			end
 			return count
 	end
 
-	def self.sum_children(pair_array,count)
-		@pair_array.each { |pair_element|  
-			if name == pair_element.name
+	def self.is_all_speaker(speaker)
+		return speaker == "ALL"
+	end
+
+	def self.sum_children(pair_array,speaker,count)
+		pair_array.each do |pair_element|  
+			if speaker == pair_element.name
 				pair_element.value += count
 				return
 			end
-		}
+		end
 		new_pair = Pair.new
-		new_pair.name = name
+		new_pair.name = speaker
 		new_pair.value = count
-		@pair_array.push(new_pair)
+		pair_array.push(new_pair)
+	end
+
+	def self.print_elements(array)
+		array.each do |element|
+			puts element.name + " " + element.value.to_s
+		end
 	end
 
 end
 
+pair_array = XML_Element_Count.parse("http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml")
+XML_Element_Count.print_elements(pair_array)
 
-XML_Element_Count.parse("http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml",
-	"SPEECH","LINE")
-
-
-
-
-
-
-pair_array = Pair_Array.new
-doc = Nokogiri::XML(open("http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"))
-speechs = doc.xpath("//SPEECH")
- speechs.each do |speech|
- 	children = speech.children
- 	if children[1].content != "ALL"
- 		pair_array.add_or_update_element(children[1].content,children.length-1)
- 	end
- 	if children[1].content == "MACBETH"
- 		puts (children.length-1).to_s
- 	end
-
- end
- pair_array.print_elements
