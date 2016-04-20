a532ad5c9503c03fd44bcb5f7276ae9aa8209b76
Vítor Alcântara de Almeida <vitoralcantara@ppgsc.ufrn.br>
2016-04-20 17:33:53 -0300

    Changed array for hash_table

--- shakespeare_analyzer.rb
+++ shakespeare_analyzer.rb
@@ -5,41 +5,35 @@ class Pair
   attr_accessor :name, :value
 end
 
-# class Pair_Array
-
-# 	def initialize
-# 		@pair_array = Array.new
-# 	end
-
-# 	def get_array
-# 		return array
-# end
-
 class XML_Element_Count
 
 	def self.parse(xml_url)
-		pair_array = Array.new		
+		pair_array = Hash.new(0)		
 		doc = Nokogiri::XML(open("http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"))
 		speeches = get_speeches(doc)
 		speeches.each do |speech|
 			children = speech.children
-			speaker = get_speaker(children)
-			if not is_all_speaker(speaker)
-				line_count = get_line_count(children)
-				sum_children(pair_array,speaker,line_count)
-			end
+			speakers = get_speakers(children)
+			line_count = get_line_count(children)
+			sum_children(pair_array,speakers,line_count)
 		end
 		return pair_array
 	end
-
-	private 
 
 	def self.get_speeches(doc)
 		return doc.xpath("//SPEECH")
 	end
 
-	def self.get_speaker(children)
-		return children[1].content
+	def self.get_speakers(children)
+		speakers = Array.new
+		children.each do |child|
+			if child.name == "SPEAKER"
+				if child.content != "ALL"
+					speakers.push(child.content)
+				end
+			end
+		end
+		return speakers
 	end
 
 
@@ -53,26 +47,15 @@ class XML_Element_Count
 			return count
 	end
 
-	def self.is_all_speaker(speaker)
-		return speaker == "ALL"
-	end
-
-	def self.sum_children(pair_array,speaker,count)
-		pair_array.each do |pair_element|  
-			if speaker == pair_element.name
-				pair_element.value += count
-				return
-			end
+	def self.sum_children(hash_table,speakers,count)
+		speakers.each do |speaker|
+			hash_table[speaker] = hash_table[speaker] + count
 		end
-		new_pair = Pair.new
-		new_pair.name = speaker
-		new_pair.value = count
-		pair_array.push(new_pair)
-	end
+	end			
 
-	def self.print_elements(array)
-		array.each do |element|
-			puts element.name + " " + element.value.to_s
+	def self.print_elements(hash_table)
+		hash_table.each do |key,value|
+			puts "#{key} #{value}"
 		end
 	end
 
