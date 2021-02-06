71da72c330593cd58ac54a283ecbfa63f0c3c5c8
Vítor Alcântara de Almeida <vit.alcantara@gmail.com>
2021-02-06 12:44:34 -0300

    Removed unnecessary code and created initial project for the new version of gratuity software

--- public/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-	<!-- Copyright -->
-	<div class="page-footer font-small blue text-center">
-		2019 - Companhia Brasileira de Trens Urbanos - CBTU<br />
-		Todos os os direitos reservados.
-	</div>
-	<!-- Copyright -->
-</footer>
