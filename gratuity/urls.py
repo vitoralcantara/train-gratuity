36549616a80024e75fc326cd97570662f785f37b
Vítor Alcântara de Almeida <vit.alcantara@gmail.com>
2021-02-06 13:06:07 -0300

    renamed project

--- gratuity/urls.py
+++ gratuity/urls.py
@@ -1,4 +1,4 @@
-"""controle_tbp URL Configuration
+"""gratuity URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'controle_tbp.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gratuity.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
